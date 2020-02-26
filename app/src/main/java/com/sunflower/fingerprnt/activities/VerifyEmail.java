f0fcb213beb7075420a6a7735ed7fb7bf84fcf43
Nurul Arifin <arifinnurul957@gmail.com>
2020-02-26 23:25:02 +0700

    Create activity ForgotPassword, VerifyEmail, and CreateNewPassword

---
+++ app/src/main/java/com/sunflower/fingerprnt/activities/VerifyEmail.java
@@ -0,0 +1,13 @@
+package com.sunflower.fingerprnt.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerifyEmail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_email);
+    }
+}
